45d5d874451ccd4627be411d49c8ec81ba7ce651
MattFreelanceWeb <matt.freelance.web@gmail.com>
2023-04-05 15:48:37 +0200

    ✨ bg red on dupplicate skills in the routine

--- components/calculator/screen/ScreenRoutine.tsx
+++ components/calculator/screen/ScreenRoutine.tsx
@@ -8,28 +8,37 @@ type Props = { showRoutine: boolean; setShowRoutine: Function; routine: any[] };
 
 function ScreenRoutine({ setShowRoutine, showRoutine, routine }: Props) {
   return (
-    <div
-      className={`h-full w-full relative grid place-items-center`}
-    >
+    <div className={`h-full w-full relative grid place-items-center`}>
       <BtnTelClose
         title={"close"}
         setShowRoutine={setShowRoutine}
         showRoutine={showRoutine}
       />
-      <ul className="w-full">
+      <ul className="w-full flex flex-col items-center justify-center gap-2">
         <li></li>
         {routine.map((item, i) => (
-          <li key={i} className="flex w-full items-center justify-between px-4">
-            <p>{item.skill}</p>
-            <p>{item.position}</p>
-            <p>{item.difficulty}</p>
+          <li
+            key={i}
+            className={`flex w-full items-center justify-between px-4 gap-2 `}
+          >
+            <div className={` w-full flex items-center justify-between rounded-md p-2 ${
+              item.difficulty === 0 &&
+              "bg-gradient-to-r from-rose-500 via-red-400 to-red-500 text-white"
+            }`}>
+              <p>{item.skill}</p>
+              <p>{item.position}</p>
+              <p>{item.difficulty}</p>
+            </div>
+
             <div className="flex items-center justify-center gap-2">
-                <BtnTelModifySkillRoutine/>
-                <BtnTelDeleteSkillRoutine/>
+              <BtnTelModifySkillRoutine />
+              <BtnTelDeleteSkillRoutine />
             </div>
           </li>
         ))}
-        <li><TotalRoutine routine={routine}/></li>
+        <li>
+          <TotalRoutine routine={routine} />
+        </li>
       </ul>
     </div>
   );
